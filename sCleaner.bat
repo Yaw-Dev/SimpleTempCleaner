4171f2e49b779283f5182c54557710ee78e5f520
A WeirD KiD <67904196+AWeirDKiD@users.noreply.github.com>
2022-06-14 17:29:18 +0300

    Update sCleaner.bat

--- sCleaner.bat
+++ sCleaner.bat
@@ -3,10 +3,10 @@
 title sCleaner
 echo Made by A WeirD KiD#2737
 
-taskkill /f /im discord
-taskkill /f /im steam
-taskkill /f /im lghub
-taskkill /f /im chrome
+taskkill /f /im discord.exe
+taskkill /f /im steam.exe
+taskkill /f /im lghub.exe
+taskkill /f /im chrome.exe
 
 rmdir /s /q %systemdrive%\Windows\Temp
 rmdir /s /q %systemdrive%\Users\%username%\AppData\Local\Temp
