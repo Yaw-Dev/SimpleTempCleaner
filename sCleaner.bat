7d5c986d2b43438a15e53850bd4612e3a2d278aa
A WeirD KiD <67904196+AWeirDKiD@users.noreply.github.com>
2022-06-14 17:26:41 +0300

    sCleaner Update

--- sCleaner.bat
+++ sCleaner.bat
@@ -3,13 +3,18 @@
 title sCleaner
 echo Made by A WeirD KiD#2737
 
-taskkill /f /im discord.exe
-taskkill /f /im steam.exe
-taskkill /f /im lghub.exe
-taskkill /f /im chrome.exe
+taskkill /f /im discord
+taskkill /f /im steam
+taskkill /f /im lghub
+taskkill /f /im chrome
 
 rmdir /s /q %systemdrive%\Windows\Temp
 rmdir /s /q %systemdrive%\Users\%username%\AppData\Local\Temp
 
 echo Empty the Recycle Bin?
 rmdir /s %systemdrive%\$Recycle.bin
+
+color 2
+echo Cleanup Successful!
+PAUSE
+EXIT
