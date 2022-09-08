84ff32f8784122d0fc3a37e619ec2d898f18dcd3
A WeirD KiD <67904196+AWeirDKiD@users.noreply.github.com>
2022-09-09 02:59:54 +0300

    Updated to V3
    Changelog:
     - Automatically seeks admin perms
     - Changed UI stuff

--- sCleaner.bat
+++ sCleaner.bat
@@ -1,20 +1,50 @@
 @echo off
 
-title sCleaner V2 - by A WeirD KiD#2737
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:-------------------------------------
+
+title sCleaner V3 - by A WeirD KiD#5573
 
 echo Are You sure you want to clean all Temp Files?
 PAUSE
+
 taskkill /f /im discord.exe
 taskkill /f /im steam.exe
-taskkill /f /im lghub.exe
 taskkill /f /im chrome.exe
+taskkill /f /im epicgameslauncher.exe
 
 rmdir /s /q %systemdrive%\Windows\Temp
 rmdir /s /q %systemdrive%\Users\%username%\AppData\Local\Temp
 
 cls
 
-echo Empty the Recycle Bin? [admin privileges required]
+echo Empty the Recycle Bin?
 rmdir /s %systemdrive%\$Recycle.bin
 
 cls
